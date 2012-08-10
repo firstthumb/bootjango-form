8156256694cd50951bb6c49e116ab0ced85a7e63
Erol KOCAMAN <kocamanerol@gmail.com>
2012-08-10 10:25:34 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='bootjango-form',
+    version='1.0.0',
+    url='https://github.com/firstthumb/bootjango-form.git',
+    author='Erol KOCAMAN',
+    author_email='kocamanerol@gmail.com',
+    license='Apache License 2.0',
+    packages=['bootjango-form', 'bootjango-form.templatetags'],
+    include_package_data=True,
+    description='Bootstrap support for Django projects',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
